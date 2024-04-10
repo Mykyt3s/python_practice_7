1b95bec172458dcb62ad8648bb9ae22f84f7f4b8
zahar <ipad32nikita@gmail.com>
2024-04-10 21:12:03 +0300

    add input and output utils

---
+++ app/io/output.py
@@ -0,0 +1,17 @@
+
+
+def console_output(text):
+    """
+    Output text to the console
+    :param text: given text
+    """
+    pass
+
+
+def file_output_builtin(file_path, text):
+    """
+    Output text to the file using builtin utils
+    :param text: given text
+    :param file_path: the path to file
+    """
+    pass
