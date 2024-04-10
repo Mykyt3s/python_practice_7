b1699a5c302f53708ac34570ce2d16e1484e88a5
zahar <ipad32nikita@gmail.com>
2024-04-10 21:12:03 +0300

    add input and output utils

---
+++ app/io/input.py
@@ -0,0 +1,25 @@
+
+
+def console_input() -> str:
+    """
+    get input text from the console
+    """
+    pass
+
+
+def read_file_builtin(file_path):
+    """
+    Reads file using builtin utils
+    :param file_path: path to the file
+    :return: file content
+    """
+    pass
+
+
+def read_file_pandas(file_path):
+    """
+    Reads file using pandas
+    :param file_path: path to the file
+    :return: file content
+    """
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,16 @@
+
+
+def console_output(text):
+    """
+    Output text to the console
+    :param text: given text
+    """
+    pass
+
+
+def file_output_builtin(text):
+    """
+    Output text to the file using builtin utils
+    :param text: given text
+    """
+    pass
